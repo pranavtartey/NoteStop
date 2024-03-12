38388c3adb37b21e84cd5b569cd7a318f30db5ac
Pranav Tartey <pranavtartey@gmail.com>
2024-03-13 00:25:34 +0530

    previous build

--- Server/app.js
+++ Server/app.js
@@ -11,16 +11,6 @@ const PORT = 8080;
 
 app.use("/notes-app/user",userRoutes);
 
-// app.post("/login", (req, res) => {
-//   console.log(req.body);
-//   res.send("You are logged in successfullly");
-// });
-
-// app.post("/register", (req, res) => {
-//   console.log(req.body);
-//   res.send("User Registered Successfully");
-// });
-
 app.get("/", (req, res) => {
   res.send("This app is working fine ;)");
 });
